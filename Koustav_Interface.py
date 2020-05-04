69720c83a443415f979ef12fa6189e13b7bc8088
koustav1908 <62374744+koustav1908@users.noreply.github.com>
2020-05-04 23:35:32 +0530

    Minor changes
    X_train and Y_train dataset are converted to numpy array before invoking function call
    copy() function is used to keep X_train and Y_train unchanged

--- Koustav_Interface.py
+++ Koustav_Interface.py
@@ -1,45 +1,42 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 import Koustav_LR
+import Koustav_NN
 
 train_df = pd.read_csv("FlightDelays.csv")
 
-X_train = train_df.drop("Flight Status", axis=1)
-Y_train = train_df["Flight Status"]
+X_train = train_df.drop("Flight Status", axis=1).to_numpy()
+Y_train = train_df["Flight Status"].to_numpy().reshape(X_train.shape[0], 1)
 
 def get_attributes(n, Model):
+	attributes, params = Model.train(X_train.copy(), Y_train.copy(), num_epoc = n)
+	return attributes
 
-	params = Model.train(X_train, Y_train, num_iter = n)
-	Y_predict = Model.predict(X_train,params)
-	acc = Model.accuracy(Y_predict, Y_train)
-	f1score, precision, recall = Model.f1score(Y_predict, Y_train)
-	return acc, f1score, precision, recall
-
-def accuracy_plots(Model) :
-	x = list(range(50,10001,50))
+def accuracy_plots() :
+	x = list(range(0,10000,50))
 	y1 = []
 	y2 = []
 	y3 = []
 	y4 = []
 	for i in x :
-		acc, f1score, precision, recall = get_attributes(i, Model)
-		y1.append(acc)
+		(train_acc, f1score, precision, recall) = get_attributes(i, Neural_Network)
+		y1.append(train_acc)
 		y2.append(f1score)
 		y3.append(precision)
 		y4.append(recall)
 	plt.figure(1)
-	plt.plot(x, y1, 'g-', label = "Accuracy")
+	plt.plot(x, y1, 'g-', label = "Train Accuracy")
 	plt.plot(x, y2, 'b-', label = "F1score")
-	plt.xlabel("iterations")
+	plt.xlabel("epoc")
 	plt.title("Learning Rate :" + str(0.001))
 	plt.legend()
 	plt.figure(2)
 	plt.plot(x, y3, 'r-', label = "Precision")
 	plt.plot(x, y4, 'b-', label = "Recall")
-	plt.xlabel("iterations")
+	plt.xlabel("epoc")
 	plt.title("Learning Rate :" + str(0.001))
 	plt.legend()
 	plt.show()
 
-accuracy_plots(Koustav_LR)
+accuracy_plots()
